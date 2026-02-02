fab212221da418d07fd9a453781e2abc851f418f
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2026-02-01 22:30:31 -0300

    project setup

---
+++ postech/postech/src/main/java/br/com/fiap/desafio/postech/PostechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.desafio.postech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechApplication.class, args);
+	}
+
+}
---
+++ postech/postech/src/test/java/br/com/fiap/desafio/postech/PostechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.desafio.postech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
